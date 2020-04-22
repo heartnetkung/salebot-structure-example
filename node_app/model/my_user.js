1c858069192799e6ccbdb6ff39c7365cbc1e53e2
heartnetkung <heartnetkung@gmail.com>
2020-04-22 16:35:12 +0700

    fix sql error

--- node_app/model/my_user.js
+++ node_app/model/my_user.js
@@ -1,9 +1,14 @@
 const { getPool } = require("../../shared/mssql/connection_pool");
+const sql = require("mssql");
 
 const selectFromUsername = async (username) => {
-	var SQL = "SELECT * FROM my_user";
+	var SQL = "SELECT * FROM my_user WHERE username = @username";
 	var pool = await getPool("db1");
-	var result = await pool.query(SQL);
+	var result = await pool
+		.request()
+		.input("username", sql.VarChar, username)
+		.query(SQL);
+	//select command return recordset
 	return result.recordset;
 };
 
